ece7a5020590713c3a583036226576db1e8594c4
amrit <info.amritpant@gmail.com>
2022-01-31 07:11:57 +0545

    Added Button Tertiary

---
+++ src/components/UI/ButtonTertiary.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function ButtonTertiary({ children, onClick }) {
+  return (
+    <Button
+      w="8.9rem"
+      h="4.8rem"
+      bgColor="#EC5757"
+      borderRadius="2.4rem"
+      color="#fff"
+      fontSize="12px"
+      fontWeight="bold"
+      letterSpacing="-0.25px"
+      marginRight="1rem"
+      onClick={onClick}
+      _hover={{ color: "#fff", bgColor: "#FF9797" }}
+      _active={{
+        color: "#fff",
+        bgcolor: "#FF9797",
+      }}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default ButtonTertiary;
