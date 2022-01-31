2ccf2fe1a2a63d7632642602136c7c8bab0b24bb
amrit <info.amritpant@gmail.com>
2022-01-31 07:15:08 +0545

    Removed the status Box logic added status box as seprate componnet

--- src/components/Main/Invoices/Invoice.js
+++ src/components/Main/Invoices/Invoice.js
@@ -3,26 +3,12 @@ import { Box, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/image";
 import "./Invoice.scss";
 import iconRightArrow from "../../../assets/icon-arrow-right.svg";
+import StatusBox from "../../UI/StatusBox";
 
 function Invoice({ id, price, clientName, dueDate, status }) {
-  let statusColor;
-  let statusBackgroundColor;
-
-  if (status === "paid") {
-    statusColor = "#33D69F";
-    statusBackgroundColor = "#33D69F0D";
-  }
-  if (status === "pending") {
-    statusColor = "#FF8F00";
-    statusBackgroundColor = "#FF8F000D";
-  }
-  if (status === "draft") {
-    statusColor = "#DFE3FA";
-    statusBackgroundColor = "#DFE3FA0D";
-  }
-
   const finalPrice = `£ ${price.toLocaleString("en-US")}`;
   const tempDate = new Date(dueDate);
+
   // prettier-ignore
   const months = ['Jan' , 'Feb' , 'Mar' , 'Apr' , 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Oct' , "Nov" ,"Dec"]
   const finalDate = `Due ${
@@ -60,32 +46,7 @@ function Invoice({ id, price, clientName, dueDate, status }) {
       >
         {finalPrice}
       </Text>
-      <Box
-        width="10.04rem"
-        h="4rem"
-        backgroundColor={statusBackgroundColor}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="6px"
-        className="status-box"
-      >
-        <Box
-          w="8px"
-          h="8px"
-          borderRadius="50%"
-          backgroundColor={statusColor}
-          mr="0.5rem"
-        ></Box>
-        <Text
-          fontSize="12px"
-          letterSpacing="-0.25px"
-          fontWeight="Bold"
-          color={statusColor}
-        >
-          {status[0].toUpperCase() + status.slice(1)}
-        </Text>
-      </Box>
+      <StatusBox status={status} />
       <Image src={iconRightArrow} />
     </Box>
   );
