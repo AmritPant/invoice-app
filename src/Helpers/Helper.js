540567bb462d416fd1d8632976ab79fbe6c6ed80
amrit Pant <info.amritpant@gmail.com>
2022-02-19 14:04:05 +0545

    Added Random Password Generator

--- src/Helpers/Helper.js
+++ src/Helpers/Helper.js
@@ -1,1 +1,11 @@
 export const BODY = document.querySelector("body");
+
+export const randomIdGenerator = function () {
+  let randomPassword;
+  const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const randomTwoLetter =
+    letter[Math.trunc(Math.random() * 26)] +
+    letter[Math.trunc(Math.random() * 26)];
+  return (randomPassword =
+    randomTwoLetter + Math.trunc(Math.random() * 9999 + 1));
+};
