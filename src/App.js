57ea7477469bd9e9dbfa851aa1dd27bd756d6c79
amrit Pant <info.amritpant@gmail.com>
2022-02-19 14:23:27 +0545

    Replaced all the contexts with react-redux

--- src/App.js
+++ src/App.js
@@ -1,61 +1,44 @@
-import React, { useState } from "react";
-import { Flex } from "@chakra-ui/react";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import FilterInvoiceContext from "./contexts/FilterInvoice";
-import Modal from "./components/Extra/Modal";
-import ModalContext from "./contexts/ModalContex";
-import InvoiceForm from "./components/Main/InvoiceForm/InvoiceForm";
-import FormContext from "./contexts/FormContext";
-import { BODY } from "./Helpers/Helper";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import InvoicePage from "./pages/InvoicePage";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Flex } from '@chakra-ui/react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import FilterInvoiceContext from './contexts/FilterInvoice';
+import Modal from './components/Extra/Modal';
+import InvoiceForm from './components/Main/InvoiceForm/InvoiceForm';
+import { BODY } from './Helpers/Helper';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import InvoicePage from './pages/InvoicePage';
 
 function App() {
   const [filterInvoiceStatus, setFilterInvoiceStatus] = useState([]);
-  const [modalStatus, setModalStatus] = useState(false);
-  const [formStatus, setFormStatus] = useState(false);
+  const formStatus = useSelector(state => state.form.formStatus);
+  const modalStatus = useSelector(state => state.modal.modalStatus);
 
   const defaultInvoiceStatus = {
     inputEls: filterInvoiceStatus,
     setFilterInvoiceStatus,
   };
 
-  const defautlModalStatus = {
-    modalStatus,
-    setModalStatus,
-  };
-
-  if (modalStatus) BODY.classList.add("overflow");
-
-  const defaultFormContext = {
-    formStatus,
-    setFormStatus,
-  };
+  if (modalStatus) BODY.classList.add('overflow');
 
   return (
     <FilterInvoiceContext.Provider value={defaultInvoiceStatus}>
-      <ModalContext.Provider value={defautlModalStatus}>
-        <FormContext.Provider value={defaultFormContext}>
-          {modalStatus && <Modal />}
-          <Flex>
-            {/* Header */}
-            <Header />
-            {/* Main */}
-            {formStatus && <InvoiceForm />}
-            <BrowserRouter>
-              <Switch>
-                <Route path="/" exact>
-                  <Main />
-                </Route>
-                <Route path="/invoices/:invoiceId">
-                  <InvoicePage />
-                </Route>
-              </Switch>
-            </BrowserRouter>
-          </Flex>
-        </FormContext.Provider>
-      </ModalContext.Provider>
+      {modalStatus && <Modal />}
+      <Flex>
+        <Header />
+        {formStatus && <InvoiceForm type="new" />}
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <Main />
+            </Route>
+            <Route path="/invoices/:invoiceId">
+              <InvoicePage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Flex>
     </FilterInvoiceContext.Provider>
   );
 }
