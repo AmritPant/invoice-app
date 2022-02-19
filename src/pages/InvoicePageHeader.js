3f0bbd1011af88bee38c2e303a9deb744465b2eb
amrit Pant <info.amritpant@gmail.com>
2022-02-19 14:18:20 +0545

    Added functionality to Delete , markAsPaid and Edit Button

--- src/pages/InvoicePageHeader.js
+++ src/pages/InvoicePageHeader.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { BODY } from "../Helpers/Helper";
 import { Flex, Text, Box } from "@chakra-ui/react";
 import StatusBox from "../components/UI/StatusBox";
 import ButtonPrimary from "../components/UI/ButtonPrimary";
 import ButtonSecondary from "../components/UI/ButtonSecondary";
 import ButtonTertiary from "../components/UI/ButtonTertiary";
 import InvoicePageGoBackLink from "./InvoicePageGoBackLink";
+import {
+  ModalPopupActions,
+  ModalSliceActions,
+  invoiceDataActions,
+  FormSliceActions,
+} from "../store/store";
 
-function InvoicePageHeader({ status }) {
+function InvoicePageHeader({ status, id }) {
+  const dispatch = useDispatch();
+
+  const deleteButtonClickHandler = () => {
+    dispatch(ModalSliceActions.setModalStatus(true));
+    dispatch(ModalPopupActions.setModalPopupStatus(true));
+  };
+
+  const markAsPaidHandler = () => {
+    dispatch(invoiceDataActions.markAsPaid(id));
+  };
+
+  const editButtonHandler = () => {
+    BODY.scrollIntoView();
+    dispatch(ModalSliceActions.setModalStatus(true));
+    dispatch(FormSliceActions.setFormStatus(true));
+  };
+
   return (
     <Box width="65%">
       <InvoicePageGoBackLink />
@@ -25,9 +50,15 @@ function InvoicePageHeader({ status }) {
           <StatusBox status={status} />
         </Flex>
         <Box>
-          <ButtonSecondary>Edit</ButtonSecondary>
-          <ButtonTertiary>Delete</ButtonTertiary>
-          <ButtonPrimary>Mark As Paid</ButtonPrimary>
+          <ButtonSecondary onClick={editButtonHandler}>Edit</ButtonSecondary>
+          <ButtonTertiary onClick={deleteButtonClickHandler}>
+            Delete
+          </ButtonTertiary>
+          {(status === "pending" || status === "draft") && (
+            <ButtonPrimary onClick={markAsPaidHandler}>
+              Mark As Paid
+            </ButtonPrimary>
+          )}
         </Box>
       </Flex>
     </Box>
