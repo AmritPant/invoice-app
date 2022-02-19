c20421fd4547787c18fda3dda5cb0712b48705eb
amrit Pant <info.amritpant@gmail.com>
2022-02-19 14:26:45 +0545

    Added Custom Input for the form

---
+++ src/components/UI/Input.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Text, Box, FormLabel, Input } from "@chakra-ui/react";
+
+const CustomInput = ({
+  name,
+  onChange,
+  id,
+  gridArea = "",
+  type = "text",
+  value = "",
+  style = {},
+  showLabel = "show",
+}) => {
+  const [isError, setError] = useState(false);
+
+  const inputBlurHandler = event => {
+    if (event.target.value.trim().length < 1) setError(true);
+  };
+
+  const inputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) setError(false);
+  };
+
+  return (
+    <Box gridArea={gridArea} style={style} className={isError ? "error" : ""}>
+      <Box display="flex" justifyContent="space-between">
+        {showLabel === "show" && <FormLabel htmlFor={id}>{name}</FormLabel>}
+        {isError && <Text>Can't be empty</Text>}
+      </Box>
+      {!onChange && (
+        <Input
+          onChange={inputChangeHandler}
+          type={type}
+          id={id}
+          defaultValue={value}
+          onBlur={inputBlurHandler}
+        />
+      )}
+      {onChange && (
+        <Input onChange={onChange} type={type} id={name} defaultValue={value} />
+      )}
+    </Box>
+  );
+};
+
+export default CustomInput;
