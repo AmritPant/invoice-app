c16dc1cd68dc4e701ad1514441d84d6738b2cd3c
amrit Pant <info.amritpant@gmail.com>
2022-02-19 14:16:37 +0545

    Added Delete Modal and removed the data import,  instead get it from store

--- src/pages/InvoicePage.js
+++ src/pages/InvoicePage.js
@@ -1,22 +1,31 @@
-import { Flex } from "@chakra-ui/react";
 import React from "react";
+import { useSelector } from "react-redux";
+import { Flex } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import InvoicePageHeader from "./InvoicePageHeader";
 import InovoicePageDetail from "./InovoicePageDetail";
-import Data from "../data.json";
+import ModalPopup from "../components/Extra/ModalPopup";
+import InvoiceForm from "../components/Main/InvoiceForm/InvoiceForm";
 
 function InvoicePage() {
+  const ModalPopupStatus = useSelector(state => state.popup.modalPopupStatus);
+  const Data = useSelector(state => state.invoice.data);
+  const InvoiceFormStatus = useSelector(state => state.form.formStatus);
+
   const params = useParams();
   const ClickedInvoice = Data.filter(
-    (invoice) => invoice.id === params.invoiceId
+    invoice => invoice.id === params.invoiceId
   ).at(0);
-
-  console.log(ClickedInvoice);
 
   return (
     <Flex flex="0.9" flexDir="column" alignItems="center" pt="4rem">
-      <InvoicePageHeader status={ClickedInvoice.status} />
+      <InvoicePageHeader
+        status={ClickedInvoice.status}
+        id={ClickedInvoice.id}
+      />
+      {InvoiceFormStatus && <InvoiceForm type="edit" data={ClickedInvoice} />}
       <InovoicePageDetail data={ClickedInvoice} />
+      {ModalPopupStatus && <ModalPopup id={ClickedInvoice.id} data={Data} />}
     </Flex>
   );
 }
